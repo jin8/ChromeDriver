f3ed9c0fda6acbc6dc75a8935c5dbb8c8b48e0e8
Brian Hong <sungjinhong@devsisters.com>
2016-06-06 13:27:48 +0900

    Update for mac. Remote wikipedia

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from selenium import webdriver
 
 from process import *
 
-quic = ""
-h2 = ""
+quic = '--origin-to-force-quic-on="login.wikimedia.org:443,ssum.casalemedia.com:443,api.branch.io:443,partner.googleadservices.com:443,en.m.wikipedia.org:443,cm.g.doubleclick.net:443,www.amazon.com:443,a.thoughtleadr.com:443,www.google-analytics.com:443,aax-us-east.amazon-adsystem.com:443,ia.media-imdb.com:443,tpc.googlesyndication.com:443,m.imgur.com:443,sync.search.spotxchange.com:443,m.imdb.com:443,shopbop.sp1.convertro.com:443,fonts.googleapis.com:443,i.imgur.com:443,ib.adnxs.com:443,sb.scorecardresearch.com:443,www.googletagservices.com:443,tap.rubiconproject.com:443,sync.1rx.io:443,pixel.quantserve.com:443,ums.adtechus.com:443,api.imgur.com:443,sync.adaptv.advertising.com:443,s.amazon-adsystem.com:443,fls-na.amazon.com:443,sync.rhythmxchange.com:443,bh.contextweb.com:443,www.apple.com:443,www.burstnet.com:443,fonts.gstatic.com:443,geo-um.btrll.com:443,t4.liverail.com:443,s.media-imdb.com:443,image5.pubmatic.com:443,www.facebook.com:443,ads.yahoo.com:443,bnc.lt:443,images-na.ssl-images-amazon.com:443,upload.wikimedia.org:443,us-u.openx.net:443"'
+h2 = "--disable-quic"
 h1 = ""
 
 
@@ -33,14 +33,13 @@ def main(mode, iterations):
         ios6ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
 
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.binary_location = "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
+        chrome_options.binary_location = "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
         chrome_options.add_argument("--incognito")
         chrome_options.add_argument("--ignore-certificate-errors")
         chrome_options.add_argument("--allow-running-insecure-content")
         chrome_options.add_argument("--enable-quic")
-        chrome_options.add_argument('--origin-to-force-quic-on="www.googletagmanager.com.this:443,s.ytimg.com.this:443,ssum.casalemedia.com.this:443,csc.beap.bc.yahoo.com.this:443,geo.yahoo.com.this:443,ecx.images-amazon.com.this:443,aax-us-east.amazon-adsystem.com.this:443,accounts.google.com.this:443,s.yimg.com.this:443,www.chinadaily.com.cn.this:443,d5nxst8fruw4z.cloudfront.net.this:443,b.scorecardresearch.com.this:443,bbs.chinadaily.com.cn.this:443,tap.rubiconproject.com.this:443,pix04.revsci.net.this:443,ums.adtechus.com.this:443,b.thumbs.redditmedia.com.this:443,www.yahoo.com.this:443,fls-na.amazon.com.this:443,www.googletagservices.com.this:443,bh.contextweb.com.this:443,adpic.chinadaily.com.cn.this:443,partner.googleadservices.com.this:443,www.google.com.this:443,ads.yahoo.com.this:443,api.imgur.com.this:443,pr-bh.ybp.yahoo.com.this:443,us-u.openx.net.this:443,www.redditmedia.com.this:443,pixel.redditmedia.com.this:443,pixel.quantserve.com.this:443,www.amazon.com.this:443,cloudfront-labs.amazonaws.com.this:443,www.redditstatic.com.this:443,ia.media-imdb.com.this:443,tpc.googlesyndication.com.this:443,www.google.co.kr.this:443,m.imdb.com.this:443,sb.scorecardresearch.com.this:443,googleads.g.doubleclick.net.this:443,c.cnzz.com.this:443,gzs20.cnzz.com.this:443,www.youtube.com.this:443,ssl.gstatic.com.this:443,geo.query.yahoo.com.this:443,sync.rhythmxchange.com.this:443,a25efb2941951158266fb235777d53410.profile.nrt53.cloudfront.net.this:443,www.burstnet.com.this:443,t4.liverail.com.this:443,image5.pubmatic.com.this:443,www.google-analytics.com.this:443,syndication.twitter.com.this:443,upload.wikimedia.org.this:443,en.m.wikipedia.org.this:443,cm.g.doubleclick.net.this:443,d31qbv1cthcecs.cloudfront.net.this:443,m.imgur.com.this:443,bnc.lt.this:443,en.wikipedia.org.this:443,same.chinadaily.com.cn.this:443,shopbop.sp1.convertro.com.this:443,i.imgur.com.this:443,s13.cnzz.com.this:443,m.youtube.com.this:443,s.amazon-adsystem.com.this:443,www.apple.com.this:443,images.apple.com.this:443,a.thoughtleadr.com.this:443,rtd.tubemogul.com.this:443,pbs.twimg.com.this:443,s.media-imdb.com.this:443,sync.adaptv.advertising.com.this:443,g-ecx.images-amazon.com.this:443,www.facebook.com.this:443,s.zkcdn.net.this:443,match.adsrvr.org.this:443,pubads.g.doubleclick.net.this:443,api.branch.io.this:443,www.reddit.com.this:443,metrics.apple.com.this:443,sync.search.spotxchange.com.this:443,mobile.twitter.com.this:443,fonts.googleapis.com.this:443,ib.adnxs.com.this:443,ma.twimg.com.this:443,engine.a.redditmedia.com.this:443,sync.1rx.io.this:443,z-ecx.images-amazon.com.this:443,reddit.com.this:443,www.gstatic.com.this:443,events.redditmedia.com.this:443,c.wrating.com.this:443,fonts.gstatic.com.this:443,geo-um.btrll.com.this:443,a.thumbs.redditmedia.com.this:443,cl2.webterren.com.this:443,ssl.google-analytics.com.this:443,images-na.ssl-images-amazon.com.this:443"')
         chrome_options.add_argument("--user-agent=" + ios6ua)
-        # chrome_options.add_argument(m)
+        chrome_options.add_argument(m)
         # chrome_options.add_argument("--disable-web-security")
         # chrome_options.add_argument("--enable-devtools-experiments")
         # driver = webdriver.Chrome(executable_path="driver/chromedriver",
